22a04d5b06d2d72d7785af679d4136ac1a4b1b8a
Veronazavr <verochka10@list.ru>
2020-11-15 23:59:33 +0300

    added updated

--- src/main/java/ru/netology/domain/Movie.java
+++ src/main/java/ru/netology/domain/Movie.java
@@ -1,18 +1,18 @@
 package ru.netology.domain;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
 
-public class Movie {
-    private int id;
-    private int productId;
-    private String productName;
-    private int productPrice;
-    private int count;
+    public class Movie {
+        private int id;
+        private int productId;
+        private String productName;
+        private int productPrice;
+        private int count;
+
 }
---
+++ src/main/java/ru/netology/manager/ManagerMovie.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Movie;
+
+public class ManagerMovie {
+
+        private Movie[] movies;
+
+        public Movie[] getMovies() {
+            //TODO: add logic
+            return null;
+        }
+    }
+
