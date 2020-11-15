732cba43ec0f053631156f50e3105f7e65353e61
Veronazavr <verochka10@list.ru>
2020-11-15 12:42:52 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Movie.java
@@ -0,0 +1,18 @@
+package ru.netology.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class Movie {
+    private int id;
+    private int productId;
+    private String productName;
+    private int productPrice;
+    private int count;
+}
